4e082788264b0f42cbeb06ac6da4d0c680fd9029
Adrian <adrian.moerchen@communardo.de>
2013-03-19 10:10:07 +0100

    Used data-intro as prefix

--- intro.js
+++ intro.js
@@ -43,8 +43,8 @@
       introItems.push({
         element: currentElement,
         intro: currentElement.getAttribute("data-intro"),
-        step: parseInt(currentElement.getAttribute("data-step"), 10),
-        position: currentElement.getAttribute("data-position") || 'bottom'
+        step: parseInt(currentElement.getAttribute("data-intro-step"), 10),
+        position: currentElement.getAttribute("data-intro-position") || 'bottom'
       });
     }
 
@@ -173,7 +173,7 @@
     tooltipLayer.style.right = null;
     tooltipLayer.style.bottom = null;
     tooltipLayer.style.left = null;
-    switch (targetElement.getAttribute('data-position')) {
+    switch (targetElement.getAttribute('data-intro-position')) {
       case 'top':
         tooltipLayer.style.left = "15px";
         tooltipLayer.style.top = "-" + (tooltipLayerPosition.height + 10) + "px";
@@ -222,9 +222,9 @@
                                            "top:"    + (elementPosition.top - 5)     + "px;" +
                                            "left: "  + (elementPosition.left - 5)    + "px;");
       //set current step to the label
-      oldHelperNumberLayer.innerHTML = targetElement.getAttribute("data-step");
+      oldHelperNumberLayer.innerHTML = targetElement.getAttribute("data-intro-step");
       //set current tooltip text
-      oldtooltipLayer.innerHTML = targetElement.getAttribute("data-intro");
+      oldtooltipLayer.innerHTML = targetElement.getAttribute("data-intro-intro");
       var oldShowElement = document.querySelector(".introjs-showElement");
       oldShowElement.className = oldShowElement.className.replace(/introjs-[a-zA-Z]+/g, '').trim();
       _placeTooltip(targetElement, oldtooltipContainer, oldArrowLayer);
@@ -247,7 +247,7 @@
       arrowLayer.className = 'introjs-arrow';
       tooltipLayer.className = "introjs-tooltip";
 
-      helperNumberLayer.innerHTML = targetElement.getAttribute("data-step");
+      helperNumberLayer.innerHTML = targetElement.getAttribute("data-intro-step");
       tooltipLayer.innerHTML = "<div class='introjs-tooltiptext'>" + targetElement.getAttribute("data-intro") + "</div><div class='introjs-tooltipbuttons'></div>";
       helperLayer.appendChild(helperNumberLayer);
       tooltipLayer.appendChild(arrowLayer);
