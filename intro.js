e602d40dd5a5c0832a37b34e367134bdc38a1a8b
kyoto <andy.nihon@gmail.com>
2013-05-02 00:04:11 +0900

    Add exitOnEsc and exitOnOverlayClick options

--- intro.js
+++ intro.js
@@ -34,7 +34,9 @@
       prevLabel: '&larr; Back',
       skipLabel: 'Skip',
       doneLabel: 'Done',
-      tooltipPosition: 'bottom'
+      tooltipPosition: 'bottom',
+      exitOnEsc: true,
+      exitOnOverlayClick: true
     };
   }
 
@@ -96,7 +98,7 @@
           nextStepButton = targetElm.querySelector('.introjs-nextbutton');
 
       self._onKeyDown = function(e) {
-        if (e.keyCode === 27) {
+        if (e.keyCode === 27 && self._options.exitOnEsc) {
           //escape key pressed, exit the intro
           _exitIntro.call(self, targetElm);
         } else if(e.keyCode === 37) {
@@ -520,9 +522,11 @@
 
     targetElm.appendChild(overlayLayer);
 
-    overlayLayer.onclick = function() {
-      _exitIntro.call(self, targetElm);
-    };
+    if(this._options.exitOnOverlayClick) {
+      overlayLayer.onclick = function() {
+        _exitIntro.call(self, targetElm);
+      };
+    }
 
     setTimeout(function() {
       styleText += 'opacity: .5;';
