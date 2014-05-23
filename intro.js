0eb38109bf9f1ad2f77cbadd3e6e193f309f0396
Afshin Mehrabani <afshin.meh@gmail.com>
2014-05-23 19:27:51 +0430

    alter intro.js version number to v0.9.0 + add minified version

--- intro.js
+++ intro.js
@@ -1,5 +1,5 @@
 /**
- * Intro.js v0.8.0
+ * Intro.js v0.9.0
  * https://github.com/usablica/intro.js
  * MIT licensed
  *
@@ -19,7 +19,7 @@
   }
 } (this, function (exports) {
   //Default config/variables
-  var VERSION = '0.8.0';
+  var VERSION = '0.9.0';
 
   /**
    * IntroJs main class
