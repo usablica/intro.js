46db672e4e6a8caa986a4c765e6028080e9552d6
Markus Doits <markus.doits@stellenticket.de>
2014-07-15 16:02:56 +0200

    Add new function `goToStepNumber`
    This allows to go to a specific step with the specific `data-step`
    attribute. This differs from `goToStep` in the way that `data-step`
    does not have be continuous to pick the desired element.
    
    For example:
    
    ```
    <div id="first" data-step='5'></div>
    <div id="second" data-step='9'></div>
    
    // to get #second one can do now:
    introJs().goToStepNumber(9); --> #second
    
    // or with goToStep:
    introJs().goToStep(2); --> #second
    ```

--- intro.js
+++ intro.js
@@ -261,6 +261,19 @@
   }
 
   /**
+   * Go to the specific step of introduction with the explicit [data-step] number
+   *
+   * @api private
+   * @method _goToStepNumber
+   */
+  function _goToStepNumber(step) {
+    this._currentStepNumber = step;
+    if (typeof (this._introItems) !== 'undefined') {
+      _nextStep.call(this);
+    }
+  }
+
+  /**
    * Go to next step on intro
    *
    * @api private
@@ -268,6 +281,16 @@
    */
   function _nextStep() {
     this._direction = 'forward';
+
+    if (typeof (this._currentStepNumber) !== 'undefined') {
+        for( var i = 0, len = this._introItems.length; i < len; i++ ) {
+            var item = this._introItems[i];
+            if( item.step === this._currentStepNumber ) {
+                this._currentStep = i - 1;
+                this._currentStepNumber = undefined;
+            }
+        }
+    }
 
     if (typeof (this._currentStep) === 'undefined') {
       this._currentStep = 0;
@@ -1123,6 +1146,10 @@
     },
     goToStep: function(step) {
       _goToStep.call(this, step);
+      return this;
+    },
+    goToStepNumber: function(step) {
+      _goToStepNumber.call(this, step);
       return this;
     },
     nextStep: function() {
