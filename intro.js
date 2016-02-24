51dffa624cab4e1961487b622a155aaf0473870c
Joey Morris <rjmorris12@gmail.com>
2016-02-24 07:21:49 -0500

    Changed version number to a valid semver value.

--- intro.js
+++ intro.js
@@ -1,5 +1,5 @@
 /**
- * Intro.js v2.0
+ * Intro.js v2.0.0
  * https://github.com/usablica/intro.js
  * MIT licensed
  *
@@ -19,7 +19,7 @@
   }
 } (this, function (exports) {
   //Default config/variables
-  var VERSION = '2.0';
+  var VERSION = '2.0.0';
 
   /**
    * IntroJs main class
