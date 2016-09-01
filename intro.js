581b38597e1ffbc9bbd7abaa192112a138fd206b
Afshin Mehrabani <afshin.meh@gmail.com>
2016-09-01 13:10:08 +0400

    removing absolute position from fixParent

--- intro.js
+++ intro.js
@@ -1060,7 +1060,7 @@
     while (parentElm != null) {
       if (!parentElm.tagName || parentElm.tagName.toLowerCase() === 'body') break;
 
-      //fix The Stacking Contenxt problem.
+      //fix The Stacking Context problem.
       //More detail: https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Understanding_z_index/The_stacking_context
       var zIndex = _getPropValue(parentElm, 'z-index');
       var opacity = parseFloat(_getPropValue(parentElm, 'opacity'));
