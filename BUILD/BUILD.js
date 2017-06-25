63a87930519b825a9dec55302aa4808750e8bdb4
Afshin Mehrabani <afshin.meh@gmail.com>
2017-06-25 10:01:01 +0430

    fixing coding style of build.js

--- BUILD/BUILD.js
+++ BUILD/BUILD.js
@@ -1,42 +1,43 @@
 #!/usr/bin/env node
 
-var fs = require("fs"), compressor = require("node-minify")
+var fs = require("fs");
+var compressor = require("node-minify");
 
 compressor.minify({
-	compressor: "gcc",
-	input: "../intro.js",
-	output: "../minified/intro.min.js",
-	callback: function(err) {
-		if (err) {
-			console.log(err)
-		} else {
-			console.log("JS minified successfully.")
-		}
-	}
+  compressor: "gcc",
+  input: "../intro.js",
+  output: "../minified/intro.min.js",
+  callback: function(err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("JS minified successfully.")
+    }
+  }
 })
 
 compressor.minify({
-	compressor: "yui-css",
-	input: "../introjs.css",
-	output: "../minified/introjs.min.css",
-	callback: function(err) {
-		if (err) {
-			console.log(err)
-		} else {
-			console.log("Main CSS minified successfully.")
-		}
-	}
+  compressor: "yui-css",
+  input: "../introjs.css",
+  output: "../minified/introjs.min.css",
+  callback: function(err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Main CSS minified successfully.")
+    }
+  }
 })
 
 compressor.minify({
-	compressor: "yui-css",
-	input: "../introjs-rtl.css",
-	output: "../minified/introjs-rtl.min.css",
-	callback: function(err) {
-		if (err) {
-			console.log(err)
-		} else {
-			console.log("RTL CSS minified successfully.")
-		}
-	}
+  compressor: "yui-css",
+  input: "../introjs-rtl.css",
+  output: "../minified/introjs-rtl.min.css",
+  callback: function(err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("RTL CSS minified successfully.")
+    }
+  }
 })
