1b8e7a4d974ffdf621d789dc3f9a08cd0ddc1be6
Ben Davis <bendavis78@gmail.com>
2021-02-09 17:47:07 -0600

    Fixes issue #1147: Positioning breaks when passing relatively-positioned element to introJs()

--- src/util/getOffset.js
+++ src/util/getOffset.js
@@ -1,22 +1,25 @@
 /**
- * Get an element position on the page
+ * Get an element position on the page relative to another element (or root)
  * Thanks to `meouw`: http://stackoverflow.com/a/442474/375966
  *
  * @api private
  * @method _getOffset
  * @param {Object} element
+ * @param {Object} relativeEl
  * @returns Element's position info
  */
-export default function getOffset(element) {
+export default function getOffset(element, relativeEl) {
   const body = document.body;
   const docEl = document.documentElement;
   const scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
   const scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+  relativeEl = relativeEl || body;
   const x = element.getBoundingClientRect();
+  const xr = relativeEl.getBoundingClientRect();
   return {
-    top: x.top + scrollTop,
+    top: x.top + scrollTop - xr.top,
     width: x.width,
     height: x.height,
-    left: x.left + scrollLeft,
+    left: x.left + scrollLeft - xr.left,
   };
 }
