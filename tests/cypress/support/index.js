1d720846f1b91eed5348de41bf180a03ced2b4c6
Afshin Mehrabani <afshin.meh@gmail.com>
2021-05-09 16:58:33 +0100

    chore(tests): lint

--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -17,7 +17,7 @@
 import './commands';
 
 Cypress.on('window:before:load', (win) => {
-  const htmlNode = win.document.querySelector('html')
+  const htmlNode = win.document.querySelector('html');
   const node = win.document.createElement('style');
   node.innerHTML = 'html { scroll-behavior: inherit !important; }';
   htmlNode.appendChild(node);
