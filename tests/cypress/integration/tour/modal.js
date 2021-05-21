3d437e6be472d1ff476666358e861ba66e057439
Afshin Mehrabani <afshin.meh@gmail.com>
2021-05-21 11:39:48 +0100

    chore: adding highlight element tests for macbook 13

--- tests/cypress/integration/tour/modal.js
+++ tests/cypress/integration/tour/modal.js
@@ -28,4 +28,37 @@ context('Modal', () => {
       cy.compareSnapshot('exit', 0);
     });
   });
+
+  it('should set the position', () => {
+    cy.window().then((win) => {
+      cy.viewport('macbook-13');
+
+      win.introJs().setOptions({
+        steps: [{
+          element: '#main-section',
+          intro: 'position bottom',
+          position: 'bottom'
+        }, {
+          element: '#clickable-button',
+          intro: 'position right',
+          position: 'right'
+        }, {
+          element: '#clickable-absolute-button',
+          intro: 'position left',
+          position: 'left'
+        }]
+      }).start();
+
+      cy.wait(800);
+      cy.compareSnapshot('position-bottom', 0);
+
+      cy.nextStep();
+      cy.wait(500);
+      cy.compareSnapshot('position-right', 0);
+
+      cy.nextStep();
+      cy.wait(500);
+      cy.compareSnapshot('position-left', 0);
+    });
+  });
 });
