b936ebf2b81a5758e4db6f054b954c388ab20a93
Afshin Mehrabani <afshin.meh@gmail.com>
2021-06-12 11:00:26 +0100

    chore: more test

--- tests/core/fetchIntroSteps.test.js
+++ tests/core/fetchIntroSteps.test.js
@@ -28,4 +28,100 @@ describe("fetchIntroSteps", () => {
     expect(steps[1].intro).toBe('second');
     expect(steps[1].step).toBe(2);
   });
+
+  test("should find the data-* elements from the DOM", () => {
+    const targetElement = document.createElement("div");
+
+    const stepOne = document.createElement('div');
+    stepOne.setAttribute('data-intro', 'first');
+
+    const stepTwo = document.createElement('div');
+    stepTwo.setAttribute('data-intro', 'second');
+    stepTwo.setAttribute('data-position', 'left');
+
+    targetElement.appendChild(stepOne);
+    targetElement.appendChild(stepTwo);
+
+    const steps = fetchIntroSteps.call(
+      {
+        _options: {
+          tooltipPosition: 'bottom'
+        },
+      },
+      targetElement
+    );
+
+    expect(steps.length).toBe(2);
+
+    expect(steps[0].position).toBe('bottom');
+    expect(steps[0].intro).toBe('first');
+    expect(steps[0].step).toBe(1);
+
+    expect(steps[1].position).toBe('left');
+    expect(steps[1].intro).toBe('second');
+    expect(steps[1].step).toBe(2);
+  });
+
+  test("should respect the custom step attribute (DOM)", () => {
+    const targetElement = document.createElement("div");
+
+    const stepOne = document.createElement('div');
+    stepOne.setAttribute('data-intro', 'second');
+    stepOne.setAttribute('data-step', '5');
+
+    const stepTwo = document.createElement('div');
+    stepTwo.setAttribute('data-intro', 'first');
+
+    targetElement.appendChild(stepOne);
+    targetElement.appendChild(stepTwo);
+
+    const steps = fetchIntroSteps.call(
+      {
+        _options: {
+          tooltipPosition: 'bottom'
+        },
+      },
+      targetElement
+    );
+
+    expect(steps.length).toBe(2);
+
+    expect(steps[0].intro).toBe('first');
+    expect(steps[0].step).toBe(1);
+
+    expect(steps[1].intro).toBe('second');
+    expect(steps[1].step).toBe(5);
+  });
+
+  test("should ignore DOM elements when options.steps is available", () => {
+    const targetElement = document.createElement("div");
+
+    const stepOne = document.createElement('div');
+    stepOne.setAttribute('data-intro', 'first');
+
+    const stepTwo = document.createElement('div');
+    stepTwo.setAttribute('data-intro', 'second');
+
+    targetElement.appendChild(stepOne);
+    targetElement.appendChild(stepTwo);
+
+    const steps = fetchIntroSteps.call(
+      {
+        _options: {
+          steps: [{
+            intro: 'steps-first',
+          }, {
+            intro: 'steps-second',
+          }]
+        },
+      },
+      targetElement
+    );
+
+    expect(steps.length).toBe(2);
+    expect(steps[0].intro).toBe('steps-first');
+    expect(steps[1].intro).toBe('steps-second');
+  });
+
+
 });
