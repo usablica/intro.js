5c43bc9f21cc8490f6b9f4764bbe694bca2c471b
Afshin Mehrabani <afshin.meh@gmail.com>
2021-11-06 09:47:47 +0000

    fix(refresh): make sure the parent layers exist

--- src/core/refresh.js
+++ src/core/refresh.js
@@ -38,12 +38,14 @@ export default function refresh(refreshSteps) {
     const oldArrowLayer = document.querySelector(".introjs-arrow");
     const oldtooltipContainer = document.querySelector(".introjs-tooltip");
 
-    placeTooltip.call(
-      this,
-      this._introItems[this._currentStep].element,
-      oldtooltipContainer,
-      oldArrowLayer
-    );
+    if (oldtooltipContainer && oldArrowLayer) {
+      placeTooltip.call(
+        this,
+        this._introItems[this._currentStep].element,
+        oldtooltipContainer,
+        oldArrowLayer
+      );
+    }
   }
 
   //re-align hints
