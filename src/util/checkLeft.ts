34e060ab084a1c28e6d09f776981b49b0b301d1d
Afshin Mehrabani <afshin.meh@gmail.com>
2023-07-03 21:44:05 +0100

    fix: better types

--- src/util/checkLeft.ts
+++ src/util/checkLeft.ts
@@ -4,10 +4,20 @@
  * @return boolean true, if tooltipLayerStyleRight is ok. false, otherwise.
  */
 export default function checkLeft(
-  targetOffset,
-  tooltipLayerStyleRight,
-  tooltipOffset,
-  tooltipLayer
+  targetOffset: {
+    top: number;
+    left: number;
+    width: number;
+    height: number;
+  },
+  tooltipLayerStyleRight: number,
+  tooltipOffset: {
+    top: number;
+    left: number;
+    width: number;
+    height: number;
+  },
+  tooltipLayer: HTMLElement
 ): boolean {
   if (
     targetOffset.left +
